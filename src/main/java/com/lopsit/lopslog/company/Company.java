5fd521b2a7a382ad5a42e85d2fae41b4103a0127
Joao Vitor Lopes <94joaovitorls@gmail.com>
2026-06-16 02:26:05 -0300

    feat: add company validation

--- src/main/java/com/lopsit/lopslog/company/Company.java
+++ src/main/java/com/lopsit/lopslog/company/Company.java
@@ -1,6 +1,7 @@
 package com.lopsit.lopslog.company;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Company {
@@ -18,6 +19,17 @@ public class Company {
       boolean active,
       LocalDateTime createdAt,
       LocalDateTime updatedAt) {
+    if (id == null) throw new IllegalArgumentException("id cannot be null");
+    if (cnpj == null) throw new IllegalArgumentException("cnpj cannot be null");
+    if (name == null || name.isBlank()) throw new IllegalArgumentException("name cannot be blank");
+    if (createdAt == null) throw new IllegalArgumentException("createdAt cannot be null");
+    if (createdAt.isAfter(LocalDateTime.now()))
+      throw new IllegalArgumentException("createdAt cannot be in the future");
+    if (updatedAt == null) throw new IllegalArgumentException("updatedAt cannot be null");
+    if (updatedAt.isAfter(LocalDateTime.now()))
+      throw new IllegalArgumentException("updatedAt cannot be in the future");
+    if (createdAt.isAfter(updatedAt))
+      throw new IllegalArgumentException("createdAt cannot be after updatedAt");
     this.id = id;
     this.cnpj = cnpj;
     this.name = name;
@@ -26,5 +38,31 @@ public class Company {
     this.updatedAt = updatedAt;
   }
 
-  // TODO
+  public static Company create(Cnpj cnpj, String name) {
+    LocalDateTime now = LocalDateTime.now();
+    return new Company(UUID.randomUUID(), cnpj, name, true, now, now);
+  }
+
+  public static Company reconstitute(
+      UUID id,
+      Cnpj cnpj,
+      String name,
+      boolean active,
+      LocalDateTime createdAt,
+      LocalDateTime updatedAt) {
+    return new Company(id, cnpj, name, active, createdAt, updatedAt);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Company company = (Company) o;
+    return Objects.equals(id, company.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
 }
--- src/test/java/com/lopsit/lopslog/company/CompanyTest.java
+++ src/test/java/com/lopsit/lopslog/company/CompanyTest.java
@@ -2,4 +2,78 @@ package com.lopsit.lopslog.company;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CompanyTest {} // TODO
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+class CompanyTest {
+  private static final Cnpj cnpj = new Cnpj("45174649000167");
+  private static final UUID randomId = UUID.randomUUID();
+  private static final LocalDateTime pastDate = LocalDateTime.now().minusDays(30L);
+  private static final LocalDateTime futureDate = LocalDateTime.now().plusDays(30L);
+
+  @Test
+  void create() {
+    // valid company
+    Company company = Company.create(cnpj, "Name");
+    assertNotNull(company);
+    // invalid cnpj
+    assertThrows(IllegalArgumentException.class, () -> Company.create(null, "Name"));
+    // invalid names
+    assertThrows(IllegalArgumentException.class, () -> Company.create(cnpj, null));
+    assertThrows(IllegalArgumentException.class, () -> Company.create(cnpj, ""));
+    assertThrows(IllegalArgumentException.class, () -> Company.create(cnpj, "\t"));
+    assertThrows(IllegalArgumentException.class, () -> Company.create(cnpj, "\n"));
+    assertThrows(IllegalArgumentException.class, () -> Company.create(cnpj, "  "));
+  }
+
+  @Test
+  void reconstitute() {
+    // valid company
+    Company company = Company.reconstitute(randomId, cnpj, "Name", true, pastDate, pastDate);
+    assertNotNull(company);
+    // invalid id
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(null, cnpj, "Name", true, pastDate, pastDate));
+    // invalid cnpj
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, null, "Name", true, pastDate, pastDate));
+    // invalid name
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, null, true, pastDate, pastDate));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, "", true, pastDate, pastDate));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, " ", true, pastDate, pastDate));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, "\t", true, pastDate, pastDate));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, "\n", true, pastDate, pastDate));
+    // invalid created at
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, "\n", true, null, pastDate));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, "\n", true, futureDate, pastDate));
+    // invalid updated at
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, "\n", true, pastDate, null));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> Company.reconstitute(randomId, cnpj, "\n", true, pastDate, futureDate));
+    assertThrows(
+        IllegalArgumentException.class,
+        () ->
+            Company.reconstitute(
+                randomId, cnpj, "\n", true, pastDate, LocalDateTime.now().minusDays(45L)));
+  }
+}
